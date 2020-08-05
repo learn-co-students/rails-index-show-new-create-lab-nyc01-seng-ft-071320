4366b89e645568e0a276712bfb30bff7b750a52b
woink <wardprice@gmail.com>
2020-08-05 17:56:58 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+5.times do 
+    Coupon.create(coupon_code: Faker::Code.nric, store: Faker::Restaurant.name )
+end
